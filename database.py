5d51d4c7c063e9e2af864853196146cdf6b5e47c
Raj Kumar Phagami <magar.fagami@gmail.com>
2023-02-27 12:17:54 -0500

    database url changed

--- database.py
+++ database.py
@@ -9,6 +9,5 @@ var_mongo_user= os.getenv('MONGO_USERNAME')
 var_mongo_pass= os.getenv('MONGO_PASS')
 var_mongodb = os.getenv('MONGO_DATABASE')
 
-
-conn = MongoClient("mongodb+srv://root:root@cluster0.t2yu0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+conn = MongoClient("mongodb+srv://root:root@cluster0.t2yu0.mongodb.net/?retryWrites=true&w=majority")
 mongodatabase = conn[var_mongodb]
